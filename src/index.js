fc9c2ab45215ee3ed1010a5b20b43c211c7a93ad
irinabondar0 <127795741+irinabondar0@users.noreply.github.com>
2024-11-19 19:44:21 +0200

    feat: working on signup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import "dotenv/config";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("It works!");
 });
+
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT, () => connectDB());
