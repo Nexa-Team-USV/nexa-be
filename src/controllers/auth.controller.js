9418bb64bbce2ad3dd063c80b663ec5794df4f54
lavylavinia <lavinia.hatnean@student.usv.ro>
2024-11-23 17:49:13 +0200

    feat: implement reset password functionality

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import { generate } from "generate-password";
 import { User } from "../models/user.model.js";
 import { generateJWT } from "../utils/generateJWT.js";
 
-const { isEmail } = validator;
+const { isEmail, isStrongPassword } = validator;
 
 export const signup = async (req, res) => {
   const { email, specialization, group, role } = req.body;
@@ -57,7 +57,7 @@ export const signup = async (req, res) => {
       numbers: true,
       symbols: true,
     });
-
+    console.log(`Password: `, password);
     // Hashes the password
     const hashed = bcrypt.hashSync(password, 10);
 
@@ -117,5 +117,49 @@ export const login = async (req, res) => {
     res.status(200).json({ token });
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  const { email, oldPassword, newPassword, confirmNewPassword } = req.body;
+
+  // Validate request body
+  if (!email || !oldPassword || !newPassword || !confirmNewPassword) {
+    return res.status(400).send('All fields are required!');
+  }
+
+  // Check if new password matches confirmation
+  if (newPassword !== confirmNewPassword) {
+    return res.status(400).send('New passwords do not match!');
+  }
+  //Check if the new password is strong
+  if (!isStrongPassword(newPassword)) {
+    return res.status(400).send('Password not strong enough!');
+  }
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).send('User not found!');
+    }
+
+    // Validate the old password
+    const isOldPasswordValid = await bcrypt.compare(oldPassword, user.password);
+    if (!isOldPasswordValid) {
+      return res.status(400).send('Old password is incorrect!');
+    }
+
+    // Hash the new password
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update the user's password
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.status(200).send('Password reset successfully!');
+  } catch (error) {
+    console.error('Error during password reset:', error);
+    res.status(500).send('Internal server error');
   }
 };
